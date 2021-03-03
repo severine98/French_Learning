3019b15281582e5e61aa8f85f671cb5497e5ff45
severine legon <severine@multiply.ai>
2021-03-03 17:53:42 +0000

    chore: first render of firebase data

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,26 @@
+// import firebase from 'react-native-firebase';
+// import database from '@react-native-firebase/database';
+// import {Platform} from 'react-native';
+
+// const iosConfig = {
+//   clientId:
+//     '1032945606148-rnlnkcnh88r5cv09lv5qauvsjblla0mb.apps.googleusercontent.com',
+//   appId:
+//     'com.googleusercontent.apps.1032945606148-rnlnkcnh88r5cv09lv5qauvsjblla0mb',
+//   apiKey: 'AIzaSyAyRaJuKAA1uDHcW_x9tAIXsbNIhvkDjY8',
+//   databaseURL:
+//     'https://french-7ad71-default-rtdb.europe-west1.firebasedatabase.app',
+//   projectId: 'french-7ad71',
+//   messagingSenderId: '1032945606148',
+//   storageBucket: 'french-7ad71.appspot.com',
+// };
+
+// // When dev for android, add config for android above and add to the bellow expression
+
+// const frenchApp = firebase.initializeApp(
+//   Platform.OS === 'ios' ? iosConfig : iosConfig,
+//   'frenchApp',
+// );
+
+// const rootRef = firebase.database().ref();
+// const nameRef = rootRef.child('name');
--- src/screens/Vocab.tsx
+++ src/screens/Vocab.tsx
@@ -1,15 +1,24 @@
-import React from 'react';
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import firebase from 'react-native-firebase';
 import {ScrollView} from 'react-native-gesture-handler';
 import {MenuTitle, NavBar} from '../../components';
-import {colors, spacing, typography} from '../styles';
-// import firestore from '@react-native-firebase/firestore';
-
-// const vocabCollection = firestore()
-//   .collection('Vocab')
-//   .doc('N4KRiij6e6Wyjotd2AO5');
+import {colors, spacing} from '../styles';
 
 export const Vocab = ({navigation}) => {
+  const [value, setValue] = useState();
+
+  console.log('value', typeof value, value);
+
+  const rootRef = firebase
+    .database()
+    .ref('age')
+    .once('value')
+    .then((snapshot) => {
+      console.log('User data: ', snapshot.val());
+      setValue(snapshot.val());
+    });
+
   const handleXmas = () => {
     navigation.navigate('Christmas');
   };
@@ -21,6 +30,8 @@ export const Vocab = ({navigation}) => {
         <Text style={styles.title}>Vocab screen</Text>
       </View>
       <View>
+        <Text>BELLO</Text>
+        <Text>{value}</Text>
         <MenuTitle title={'Christmas'} onPress={handleXmas} />
         <MenuTitle title={'Holidays'} onPress={handleXmas} />
         <MenuTitle title={'Cooking'} onPress={handleXmas} />
