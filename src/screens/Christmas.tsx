024d4a959dab2c6a8b09ac8c4bcb93baa769a237
severine legon <severine@multiply.ai>
2021-03-03 23:01:22 +0000

    feat: vocab card style tweaks

--- src/screens/Christmas.tsx
+++ src/screens/Christmas.tsx
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import firebase from 'react-native-firebase';
 import {ScrollView} from 'react-native-gesture-handler';
+import {SafeAreaView} from 'react-native-safe-area-context';
 import {NavBar, VocabCard} from '../../components';
 import {colors, spacing} from '../styles';
 
@@ -24,41 +25,44 @@ export const Christmas = ({navigation}) => {
   }, []);
 
   return (
-    <ScrollView style={{flex: 1}} contentContainerStyle={{flexGrow: 1}}>
-      <NavBar />
-      <View style={styles.titleContainer}>
-        <Text style={styles.title}>Christmas vocab</Text>
-        {Object.entries(frenchWord ?? {}).map((word) => {
-          return (
-            <VocabCard
-              sentenceEnglish={word[1].sentenceEnglish}
-              sentenceFrench={word[1].sentenceFrench}
-              wordFrench={word[1].wordFrench}
-              wordEnglish={word[1].wordEnglish}
-            />
-          );
-        })}
-        <TouchableOpacity
-          style={{backgroundColor: colors.white}}
-          onPress={handleTest}>
-          <Text style={{textAlign: 'center', padding: spacing.base}}>
-            Take the test
-          </Text>
-        </TouchableOpacity>
-      </View>
-    </ScrollView>
+    <SafeAreaView style={{flex: 1}}>
+      <ScrollView style={{flex: 1}} contentContainerStyle={{flexGrow: 1}}>
+        <NavBar />
+        <View style={styles.titleContainer}>
+          <Text style={styles.title}>Christmas vocab</Text>
+          {Object.entries(frenchWord ?? {}).map((word) => {
+            return (
+              <VocabCard
+                sentenceEnglish={word[1].sentenceEnglish}
+                sentenceFrench={word[1].sentenceFrench}
+                wordFrench={word[1].wordFrench}
+                wordEnglish={word[1].wordEnglish}
+              />
+            );
+          })}
+          <TouchableOpacity
+            style={{backgroundColor: colors.white}}
+            onPress={handleTest}>
+            <Text style={{textAlign: 'center', padding: spacing.base}}>
+              Take the test
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  titleContainer: {
+    flex: 1,
+    backgroundColor: colors.green,
+    marginBottom: spacing.large,
+  },
   title: {
     fontSize: 30,
     color: colors.white,
     textAlign: 'center',
     padding: spacing.base,
-  },
-  titleContainer: {
-    backgroundColor: colors.green,
-    marginBottom: spacing.large,
   },
 });
